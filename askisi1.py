207a12fbe54e1e35fb453c8029d5b4a389b9d8d2
missginger <mariadimakogianni@gmail.com>
2018-01-10 18:29:36 +0200

    teliko askisi 8

---
+++ askisi1.py
@@ -0,0 +1,7 @@
+from random import shuffle
+
+    for i in range(1000):
+        for i in range(1, 81):
+            shuffle(x)
+        la[i] = x
+        print ran[i]
--- askisi8.py
+++ askisi8.py
@@ -1,14 +1,22 @@
 import random
 ran=[0]*30
+lis=[]
+prod=[]
 for i in range (30):
     ran[i]=random.randint(-30,30)
 p=0
-for i in range (30):
-  for j in range (30):
-    for k in range(30):
+for i in ran:
+  for j in ran:
+    for k in ran:
       if i!=j and j!=k and k!=i:
-         if ran[i]+ran[j]+ran[k]==0:
-           print (ran[i],ran[k],ran[j])
+         if i+j+k==0:
+           pr=[i,j,k]
+           gin = i*j*k
+           if not gin in prod:
+               prod.append(gin)
+               lis.append(pr)
            p=1
 if p==0:
     print("den yparxei tetoia triada")
+else:
+    print lis
