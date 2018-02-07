884957e651d8a3d5846de58e25c3a211b4d09252
missginger <mariadimakogianni@gmail.com>
2018-02-07 18:56:22 +0200

    final

--- askisi1.py
+++ askisi1.py
@@ -3,21 +3,25 @@ bingo = ['b','b','b','b','b']
 arithmoi=[]
 for m in range (1000):
     k=0
-    paiktis = []
+    paiktes = []
     for i in range (100):
-        num = []
-        for j in range (5):
+         paiktis = []
+         while len(paiktis)<5:
             x=random.randint(1, 80)
-            num.append(x)
-        paiktis.append(num)
-    while bingo not in paiktis:
-        k=k+1
+            if x not in paiktis:
+                paiktis.append(x)
+         paiktes.append(paiktis)
+    noep=[]
+    while bingo not in paiktes:
         y=random.randint(1,80)
-        for i in range(100) :
-         for j in range(5):
-          if y == paiktis[i][j]:
-              paiktis[i][j]="b"
+        if y not in noep:
+            k=k+1
+            noep.append(y)
+            for i in range(100):
+                for j in range(5):
+                    if y == paiktes[i][j]:
+                        paiktes[i][j]="b"
     arithmoi.append(k)
 lsum=sum(arithmoi)
-mo= lsum/1000
+mo= lsum/1000.0
 print 'o mesos oros arithmon pou prepei na anagkelthoyn gia na ginei bingo einai' ,mo
