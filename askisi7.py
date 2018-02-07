51383a834bd55506a2d8f800a52e2379ccdca8e7
missginger <mariadimakogianni@gmail.com>
2018-02-07 16:49:48 +0200

    final

--- askisi7.py
+++ askisi7.py
@@ -6,8 +6,8 @@ day= now.day
 daywe=datetime.datetime.today().weekday()
 p=0
 for x in range (year+1, year+11):
-    for j in range (1,12):
-        future=datetime.datetime(x, month, day)
+    for j in range (1,13):
+        future=datetime.datetime(x, j, day)
         if daywe==future.weekday():
             p=p+1
 print p
