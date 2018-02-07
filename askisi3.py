8e943f98541c4b64d9735a31bf8db5597510b4d7
missginger <mariadimakogianni@gmail.com>
2018-02-07 16:41:30 +0200

    final

--- askisi3.py
+++ askisi3.py
@@ -4,9 +4,15 @@ string=raw_input("dose string" )
 p=list(string)
 x=len(p)
 for  i in range (x):
-    if chr(ord(p[i]))<chr(ord("n")):
-       p[i]=chr(ord(p[i]) +13)
-    else:
-        p[i]=chr(ord(p[i]) -13)
+    if chr(ord(p[i]))>=chr(ord("A")) and chr(ord(p[i]))<=chr(ord("Z")):
+        if chr(ord(p[i]))<chr(ord("N")):
+           p[i]=chr(ord(p[i]) +13)
+        else:
+            p[i]=chr(ord(p[i]) -13)
+    elif chr(ord(p[i]))>= chr(ord("a")) and chr(ord(p[i])) <= chr(ord("z")):
+        if chr(ord(p[i]))<chr(ord("n")):
+            p[i]=chr(ord(p[i]) +13)
+        else:
+            p[i]=chr(ord(p[i]) -13)
 final=''.join(p)
 print final
